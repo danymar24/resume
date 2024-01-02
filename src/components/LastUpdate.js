7ed91f8f5f796c0945a59d75344bb35e06830ead
Daniel Rodriguez <danymar24@gmail.com>
2024-01-02 12:09:34 -0600

    Updating resume Adding deploy and build workflows

---
+++ src/components/LastUpdate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card, CardContent } from "@material-ui/core";
+
+function LastUpdate() {
+    return (
+        <Card>
+            <CardContent>
+                <h2>Updated on: 1/2/2024</h2>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default LastUpdate;
